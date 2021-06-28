d9fe8dd94dcb658aad72dd8b93e530e9d5341986
Nikolay Rademacher <hrrdx33@gmail.com>
2021-06-28 23:00:41 +0200

    feat: add about page content

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,7 +5,33 @@ export default function About() {
   return (
     <>
       <NavBar />
-      <main className="container mb-8">Coming Soon</main>
+      <main className="mt-8 mb-32 container">
+        <header className="font-heading text-center mb-16">
+          <h1 className="text-5xl font-bold">About</h1>
+        </header>
+        <section className="row items-center justify-around">
+          <article className="md:col-8 space-y-4 text-xl leading-relaxed">
+            <p>
+              My name is Nikolay Rademacher and I'm the owner of Rademacher
+              Solutions.
+            </p>
+            <p>
+              I created Rademacher Solutions to combine two passions of mine:
+              Building expressive, high-quality web solutions and making them
+              available to as many people as possible
+            </p>
+            <p>
+              That is why I've made it my mission to offer exclusive web
+              products at inclusive prices.
+            </p>
+          </article>
+          <img
+            className="hidden md:block md:col-4 mask mask-star-2 rotate-6"
+            src="https://avatars.githubusercontent.com/u/47133234?v=4"
+            alt="Nikolay Rademacher"
+          />
+        </section>
+      </main>
       <Footer />
     </>
   );
